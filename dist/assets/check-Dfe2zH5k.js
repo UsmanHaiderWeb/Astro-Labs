ef07ffcf3e6a952946969e157570579aba58db8d
UsmanHaiderWeb <usmanhaiderweb2005@gmail.com>
2025-03-15 15:10:23 +0500

    completed ui with build

--- dist/assets/check-Dfe2zH5k.js
+++ dist/assets/check-Dfe2zH5k.js
@@ -1,4 +1,4 @@
-import{c}from"./index-BgMoaJc6.js";/**
+import{c}from"./index-ChFwzRQ4.js";/**
  * @license lucide-react v0.479.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- dist/assets/chevron-down-lUY9EhUg.js
+++ dist/assets/chevron-down-lUY9EhUg.js
@@ -1,4 +1,4 @@
-import{c as o}from"./index-BgMoaJc6.js";/**
+import{c as o}from"./index-ChFwzRQ4.js";/**
  * @license lucide-react v0.479.0 - ISC
  *
  * This source code is licensed under the ISC license.
